98705fb7398ffd1dfe7b11a12f5ad592ca774db4
govardhan-26 <bgrnaidu@gmail.com>
2023-08-05 12:44:24 +0530

    cahnges1

--- client/src/store/useAuthStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand'
-
-const useAuthStore = create((set) => ({
-    auth: null,
-    setAuth: (auth) => set({ auth }),
-    logout: () => set({ auth: null }),
-}))
-
-export default useAuthStore
